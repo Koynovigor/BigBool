d983de34802654aea1968658c18504cd0b1f1f9d
Koynovigor <igorkoynov2001@gmail.com>
2020-05-02 05:50:59 +0700

    02.05

--- bool.c
+++ bool.c
@@ -5,8 +5,40 @@
 #include <string.h>
 
 
-struct bigbool* conversion (char* vector, int len, struct bigbool war)
+
+char* scan_vector()
 {
+    int count = 0;
+    int count_max = 128;
+    int symbol;
+    char* str = calloc(count_max, sizeof(char));
+    if (str == NULL)
+        {
+            return NULL;
+        }
+    while ((symbol = fgetc(stdin)) != '\n')
+    {
+        str[count] = symbol;
+        count ++;
+        if (count == count_max)
+        {
+            str = (char*)realloc(str, count_max + 128);
+            if (str == NULL)
+            {
+                return NULL;
+            }
+            count_max += 128;
+        }
+    }
+return str;
+}
+
+
+struct bigbool* conversion (char* vector)
+{
+    int len = strlen(vector);
+    struct bigbool* war = calloc(1, sizeof(struct bigbool));
+    
     int j = -1;            
     for (int i = 0; i < len; i++)
     {
--- bool.h
+++ bool.h
@@ -1,5 +1,16 @@
+#include <inttypes.h>
 #ifndef BOOL_H
 #define BOOL_H
+
+
+struct bigbool 
+    {
+        uint8_t* parts;
+        int last_bit;
+        int last_byte;
+    };
+
+char* scan_vector();
 
 
 
